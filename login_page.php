7a1df08375f628a08a3080f5279252f810841140
Ahmed-Al-Mohammadi <ahmedyassersayed2024@gmail.com>
2024-09-23 08:16:42 +0300

    Login & Registration

---
+++ login_page.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="login2.css">
+    <title>Login & Registration</title>
+</head>
+<body>
+ <div class="wrapper">
+    <nav class="nav">
+        <div class="nav-logo">
+            <p>LOGO .</p>
+        </div>
+        <div class="nav-menu" id="navMenu">
+            <ul>
+                <li><a href="#" class="link active">Home</a></li>
+                <li><a href="#" class="link">Blog</a></li>
+                <li><a href="#" class="link">Services</a></li>
+                <li><a href="#" class="link">About</a></li>
+            </ul>
+        </div>
+        <div class="nav-button">
+            <button class="btn white-btn" id="loginBtn" onclick="login()">Sign In</button>
+            <button class="btn" id="registerBtn" onclick="register()">Sign Up</button>
+        </div>
+        <div class="nav-menu-btn">
+            <i class="bx bx-menu" onclick="myMenuFunction()"></i>
+        </div>
+    </nav>
+<!----------------------------- Form box ----------------------------------->  
+    <form method="POST">
+    <div class="form-box">
+        
+        <!------------------- login form -------------------------->
+        
+        <div class="login-container" id="login">
+            <div class="top">
+                <span>Don't have an account? <a href="#" onclick="register()">Sign Up</a></span>
+                <header>Login</header>
+            </div>
+            <div class="input-box">
+                <input type="text" class="input-field" placeholder="Username or Email" name="username">
+                <i class="bx bx-user"></i>
+            </div>
+            <div class="input-box">
+                <input type="password" class="input-field" placeholder="Password" name="password">
+                <i class="bx bx-lock-alt"></i>
+            </div>
+            <div class="input-box">
+                <input type="submit" class="submit" value="Sign In" name="login">
+            </div>
+            <div class="two-col">
+                <div class="one">
+                    <input type="checkbox" id="login-check">
+                    <label for="login-check"> Remember Me</label>
+                </div>
+                <div class="two">
+                    <label><a href="#">Forgot password?</a></label>
+                </div>
+            </div>
+        </div>
+        
+        <!------------------- registration form -------------------------->
+        <div class="register-container" id="register">
+            <div class="top">
+                <span>Have an account? <a href="#" onclick="login()">Login</a></span>
+                <header>Sign Up</header>
+            </div>
+            <div class="two-forms">
+                <div class="input-box">
+                    <input type="text" class="input-field" placeholder="Firstname">
+                    <i class="bx bx-user"></i>
+                </div>
+                <div class="input-box">
+                    <input type="text" class="input-field" placeholder="Lastname">
+                    <i class="bx bx-user"></i>
+                </div>
+            </div>
+            <div class="input-box">
+                <input type="text" class="input-field" placeholder="Email">
+                <i class="bx bx-envelope"></i>
+            </div>
+            <div class="input-box">
+                <input type="password" class="input-field" placeholder="Password">
+                <i class="bx bx-lock-alt"></i>
+            </div>
+            <div class="input-box">
+                <input type="submit" class="submit" value="Register">
+            </div>
+            <div class="two-col">
+                <div class="one">
+                    <input type="checkbox" id="register-check">
+                    <label for="register-check"> Remember Me</label>
+                </div>
+                <div class="two">
+                    <label><a href="#">Terms & conditions</a></label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</form>   
+<script>
+   
+   function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+ 
+</script>
+<script>
+    var a = document.getElementById("loginBtn");
+    var b = document.getElementById("registerBtn");
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+    function login() {
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        a.className += " white-btn";
+        b.className = "btn";
+        x.style.opacity = 1;
+        y.style.opacity = 0;
+    }
+    function register() {
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        a.className = "btn";
+        b.className += " white-btn";
+        x.style.opacity = 0;
+        y.style.opacity = 1;
+    }
+</script>
+</body>
+</html>
+<?php
+$username = "ahmed";
+$password = "123";
+
+$username2 = "yasser";
+$password2 = "456";
+
+$username3 = "sayed";
+$password3 = "789";
+
+$username4 = "omar";
+$password4 = "147";
+
+$username5 = "mamdoh";
+$password5 = "258";
+
+$username6 = "eslam";
+$password6 = "369";
+
+if(isset($_POST['login'])){
+$getUserName = $_POST['username'];
+$getpassword = $_POST['password'];
+
+if($username === $getUserName && $password === $getpassword){
+    
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+}
+elseif($username2 === $getUserName && $password2 === $getpassword){
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+
+}
+elseif($username3 === $getUserName && $password3 === $getpassword){
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+
+}
+elseif($username4 === $getUserName && $password4 === $getpassword){
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+}
+
+elseif($username5 === $getUserName && $password5 === $getpassword){
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+
+}
+
+elseif($username6 === $getUserName && $password6 === $getpassword){
+    $_SESSION['USER'] = $getUserName;
+    $_SESSION['PASSWORD'] = $getpassword;
+    $_SESSION['LOGIN'] = true;
+    echo " <script> location.replace('index2.php') </script>";
+
+}
+
+else{
+    echo "Checked your username or password";
+}
+
+}
+?>
